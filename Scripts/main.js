e19fb12699d65aa6eda847ea4b0d995fef07bb6a
alien <148527832+itsalien024@users.noreply.github.com>
2023-12-24 16:58:22 +0300

    Working Dynamic design for adding/deleting/marking read or undread

--- Scripts/main.js
+++ Scripts/main.js
@@ -4,6 +4,7 @@ let closeBtn = document.querySelector(".closeModal");
 let addBtn = document.querySelector(".addBook");
 let read = document.getElementById("read-check");
 let form = document.querySelector(".book-form");
+let readBtn = document.querySelector(".read-button");
 
 let container = document.querySelector(".library-container");
 let title = document.getElementById("title");
@@ -34,35 +35,75 @@ addBtn.onclick = function() {
 }
 
 function newBook() {
-  let newBook = title.value;
-  newBook = new book(title.value, author.value, pageCount.value, read.value);
+  if (read.checked == true) {
+    read.value = "Yes";
+  }else {
+    read.value = "No";
+  }
+
+  let newBook = new book(title.value, author.value, pageCount.value, read.value);
   library.push(newBook);
-  
+
+  createCard(newBook);
+}
+
+function createCard(book) {
   let newBookCard = document.createElement("div");
   newBookCard.classList.add("book-card");
 
   newBookCard.innerHTML = `
   <span class="remove-card">Delete</span>
   <span class="indicator">Title</span>
-  <div class="card-title"><span class="inner-info"></span></div>
+  <div class="card-title"><span class="inner-info">${book.title}</span></div>
   <span class="indicator">Author</span>
-  <div class="card-author"><span class="inner-info"></span></div>
+  <div class="card-author"><span class="inner-info">${book.author}</span></div>
   <span class="indicator">pages</span>
-  <div class="card-pages"><span class="inner-info"></span></div>
+  <div class="card-pages"><span class="inner-info">${book.pages}</span></div>
   <span class="indicator">Have you read this book?</span>
-  <div class="card-read"><span class="inner-info"></span></div>
+  <div class="card-read"><button class="read-button">${read.value}</button></div>
   `;
 
   container.appendChild(newBookCard);
 
+  let newReadBtn = newBookCard.querySelector(".read-button");
+  let deleteBtn = newBookCard.querySelector(".remove-card");
 
+  if (read.value == "Yes") {
+    newReadBtn.textContent = "Yes";
+  }else if (read.value == "No") {
+    newReadBtn.textContent = "No";
+    newReadBtn.classList.add("unread-button");
+  }
+
+  newReadBtn.onclick = function() {
+    if (newReadBtn.textContent == "Yes") {
+      newReadBtn.textContent = "No";
+      newReadBtn.classList.remove("read-button");
+      newReadBtn.classList.add("unread-button") ;
+    }else if (newReadBtn.textContent == "No") {
+      newReadBtn.textContent = "Yes";
+      newReadBtn.classList.remove("unread-button");
+      newReadBtn.classList.add("read-button");
+    }
+  }
+
+  deleteBtn.onclick = function() {
+    newBookCard.remove();
+  }  
+
+} 
+
+readBtn.onclick = function() {
+  if (readBtn.textContent == "Yes") {
+    readBtn.textContent = "No";
+    readBtn.classList.remove("read-button");
+    readBtn.classList.add("unread-button") ;
+  }else if (readBtn.textContent == "No") {
+    readBtn.textContent = "Yes";
+    readBtn.classList.remove("unread-button");
+    readBtn.classList.add("read-button");
+  }
 }
-
-for (let books of library) {
-
-}
-
-
 
 
 
